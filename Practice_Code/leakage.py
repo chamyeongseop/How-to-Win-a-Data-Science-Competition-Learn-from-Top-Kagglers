890025979707185ea95d346b0cdb40f1c2945c43
Myeongseop Cha <chamysub@gmail.com>
2019-10-24 17:05:38 +0900

    time lag

--- Practice_Code/leakage.py
+++ Practice_Code/leakage.py
@@ -14,3 +14,14 @@ train = sales.join(items, on ='item_id', rsuffix='_').join(shops, on='shop_id', 
 # axis = 0의 값을 rowf(행)를 기준으로 계산 및 함수를 적용한다. (x축)
 # axis = 1은 값을 column(열)을 기즌으로 계산 및 함수를 적용한다. (y축)
 # axis = 2는 값을 Z축 기준으로 계산 및 함수를 적용한다.
+
+# Time lag 설정
+
+time_shift = [1,2,3,4,5,6]
+
+for lag in time_shift:
+    ft_name = ('item_cnt_shifted%s' % lag)
+    train_monthly[ft_name] = train_monthly.sort_values('date_block_name').groupby(['shop_id', 'item_category_id', 'item_id'])['item_cnt'].shift(lag)
+
+    # Fill the empty shifted features with 0
+    train_monthly[ft_name].fillna(0, inplace = True)
